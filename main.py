219a92181deaaf40439e84d3fbd3cc172b2e2651
MrCaligari <yuramishcherskiy@mail.ru>
2023-02-06 17:26:54 +0300

    Решение по задаче с Фибоначчи(2)

--- main.py
+++ main.py
@@ -1,61 +1,153 @@
 # import  testfile
 # testfile.printsale()
-def number(num):
-    mylist = []
-    prev = 0
-    new = 1
-    summ = 0
-    while num > summ:
-        summ = prev + new
-        new = prev
-        prev = summ
-        mylist.append(summ)
-    if num in mylist:
-        return mylist.index(num)
-    else:
-        return "Такого числа нет в последовательности"
-
-num = int(input())
-
-print(number(num))
+# def number(num):
+#     mylist = []
+#     prev = 0
+#     new = 1
+#     summ = 0
+#     while num > summ:
+#         summ = prev + new
+#         new = prev
+#         prev = summ
+#         mylist.append(summ)
+#     if num in mylist:
+#         return mylist.index(num)
+#     else:
+#         return "Такого числа нет в последовательности"
+#
+# num = int(input())
+#
+# print(number(num))
 # Фибоначчи вывести, порядковый номер числа(например 8)
-
+import random
 # Написать функцию, которая по входному числу рисует букву w
 # Например *              *               *
 #           *       *       *       *
 #            *               * - если пришло число 3(через пробел, не через TAB)
 
 
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
-# объявление функции
+# from random import *
+# r = randint(1, 100)
+# count = 0
+# while True:
+#     count += 1
+#     n = input()
+#     if n.isdigit() and int(n) > r:
+#         print("Ваше число больше загаданного")
+#     elif n.isdigit() and int(n) < r:
+#         print("Ваше число меньше загаданного")
+#     elif n.isdigit() and int(n) == r:
+#         print("Вы угадали с", count, "попытки!")
+#         print("Введите число еще раз!")
+#     else:
+#         print("Введите число заново")
 
-# объявление функции
-# def is_valid_password(password):
-#     cnt = 0
-#     password = password.split(":")
-#     if len(password) == 3:
-#         cnt += 1
-#     if password[0] == password[0][::-1]:
-#         cnt += 1
-#     total = 0
-#     for i in range(1, int(password[1]) + 1):
-#         if int(password[1]) % i == 0:
-#             total += 1
-#     if total == 2:
-#         cnt += 1
-#     if int(password[2]) % 2 == 0:
-#         cnt += 1
-#     return cnt == 4
+# class dogs:
+#     fur = True
+#     paw_count = 4
+#     tail = 1
 #
-# # считываем данные
-# psw = input()
+# colly = dogs()
+# print(colly.fur)
+# sharik = dogs()
+# print(sharik.fur)
+
+
+# class cars:
+#     speed = 0
+#     kolesa = 4
+#     def voice(self):
+#         print("beep")
 #
-# # вызываем функцию
-# print(is_valid_password(psw))
+#
+# ford = cars()
+# print(ford.kolesa)
+#
+# mazda = cars()
+# print(mazda.kolesa)
+#
+# volkswagen = cars()
+# print(volkswagen.kolesa)
+#
+# mercedes = cars()
+# print(mercedes.kolesa)
+#
+# mazda.voice()
+# mercedes.voice()
+#
+# mazda.morespeed()
+
+# from random import *
+# answers = ["Бесспорно", "Предрешено", "Никаких сомнений", "Определённо да","Можешь быть уверен в этом", "Мне кажется - да", "Вероятнее всего", "Хорошие перспективы", "Знаки говорят - да", "Да", "Пока неясно, попробуй снова", "Спроси позже", "Лучше не рассказывать", "Сейчас нельзя предсказать", "Сконцентрируйся и спроси опять", "Даже не думай", "Мой ответ - нет", "По моим данным - нет", "Перспективы не очень хорошие", "Весьма сомнительно"]
+# print('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
+# print("Введи свое имя")
+# name = input()
+# print("Привет,", name)
+# while True:
+#     print("Задайте Ваш вопрос шару")
+#     ques = input()
+#     print(choice(answers))
+#     print("Хотите ли Вы задать еще один вопрос? Ответьте Да или Нет")
+#     ques2 = input()
+#     if ques2 == "Да" or ques2 == "да":
+#         continue
+#     elif ques2 == "Нет" or ques2 == "нет":
+#         print("Возвращайся, если возникнут вопросы!")
+#         break
+#     else:
+#         print("Введите Да или Нет")
+
+# from random import *
+# digits = '0123456789'
+# lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+# uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# punctuation = '!#$%&*+-=?@^_'
+# chars = ''
+# print("Сколько паролей необходимо сгенерировать?")
+# much = int(input())
+# print("Длина пароля")
+# length = int(input())
+# print("Включать ли цифры 0123456789?")
+# isdigits = input()
+# print("Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?")
+# isuppercase = input()
+# print("Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?")
+# islowercase = input()
+# print("Включать ли символы !#$%&*+-=?@^_?")
+# issymbols = input()
+# while True:
+#     if isdigits.lower() == "да":
+#         chars += digits
+#     else:
+#         input("Ответьте да или нет")
+#     if isuppercase == "да":
+#         chars += uppercase_letters
+#     else:
+#         input("Ответьте да или нет")
+#     if islowercase == "да":
+#         chars += lowercase_letters
+#     else:
+#         input("Ответьте да или нет")
+#     if issymbols == "да":
+#         chars += punctuation
+#     break
+
+# def generate_password(chars, length):
+#     return sample(chars, length)
+#
+#
+# for i in range(int(much)):
+#     print(*generate_password(chars, length), sep="")
+
+def number(num):
+    mylist = []
+    prev = 0
+    new = 1
+    summ = 0
+    for i in range(num):
+        summ = prev + new
+        new = prev
+        prev = summ
+    return new
+
+print(number(int(input())))
